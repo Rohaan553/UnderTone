3b288a4ae6345e7bf1a05c2888b959df1517f46f
Vivek Rajkumar <vrr11@pitt.edu>
2023-03-19 04:55:21 -0400

    change logic for enabling/disabling clear button

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,15 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TextInput, Pressable, useColorScheme} from 'react-native';
 
 import { SafeAreaView } from 'react-native-safe-area-context';
 const HomePage = () => {
 	const [placeholderText, setPlaceHolderText] = useState("Type or paste your text here...");
 	const [inputText, setInputText] = useState("");
+	const [isDisabled, setIsDisabled] = useState(true);
 	const theme = useColorScheme(); // import device's color scheme (dark mode or light mode)
+	
+	// enable or disable clear button based on status of inputText
+	useEffect(() => {
+		if (inputText.length > 0) {
+			console.log("enabling clear button");
+			setIsDisabled(false);
+		} else {
+			console.log("disabling clear button");
+			setIsDisabled(true);
+		}
+	}, [inputText]);
 
     return ( 
         
-            <View style={theme == 'light' ? styles.container : styles.containerDark}> 
+        <View style={theme == 'light' ? styles.container : styles.containerDark}> 
             
             <SafeAreaView style={theme == 'light' ? styles.container : styles.containerDark}>
             <Text style={theme == 'light' ? styles.titleText : styles.titleTextDark}>UnderTone</Text>  
@@ -18,50 +30,50 @@ const HomePage = () => {
                 multiline= {true}
                 numberOfLines={7}
                 placeholder={placeholderText}
-				placeholderTextColor="#ECEDEE"
-				onFocus={(focusEvent) => {
+				placeholderTextColor={theme == 'light' ? 'grey' : 'lightgray'}
+				onFocus={focusEvent => {
 					console.log("text box focused");
 					setPlaceHolderText("");
 				}}
 				value={inputText}
 				onChangeText={text => {
-					console.log(text);
 					setInputText(text);
 				}}
             />
+			
             <Pressable
-                style={({pressed}) => [styles.unPressedButton,
+                style={({ pressed }) => [styles.unPressedButton,
                     {
-                    backgroundColor: pressed ? 'hsl(294, 35%, 35%)' : '#46024E',
-                    },
-                    
+                      backgroundColor: pressed ? 'hsl(294, 35%, 35%)' : '#46024E',
+                    }
                 ]}
 				onPress={pressEvent => {
 					if (inputText.length == 0) {
 						alert("Please enter some text first!");
 					}
 				}}>
+				
                 <Text style={styles.buttonText}>Analyze</Text>
             </Pressable>
 			
 			<Pressable
-                style={({pressed}) => [styles.unPressedButton,
+                style={({ pressed }) => [styles.unPressedButton,
                     {
-                    backgroundColor: pressed ? 'hsl(294, 35%, 35%)' : '#46024E',
-                    },
-                    
+                      backgroundColor: pressed ? 'hsl(294, 35%, 35%)' : '#46024E',
+                    }
                 ]}
 				onPress={pressEvent => {
 					console.log("pressed clear");
 					setInputText("");
 					setPlaceHolderText("Type or paste your text here...");
 				}}
-				disabled={!(inputText.length > 0)}>
+				disabled={isDisabled}>
+				
                 <Text style={styles.buttonText}>Clear</Text>
             </Pressable>
 
             </SafeAreaView>
-         </View>
+        </View>
        
        
      );
